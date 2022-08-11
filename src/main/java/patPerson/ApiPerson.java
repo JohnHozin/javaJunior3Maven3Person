aa6dc02ec635a75eb74fedb3eda9fba8b14bf8a7
Evgen <simple_protector@mail.ru>
2022-08-11 12:09:08 +0300

    Change files

--- src/main/java/patPerson/ApiPerson.java
+++ src/main/java/patPerson/ApiPerson.java
@@ -1,10 +1,13 @@
 package patPerson;
 
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.time.ZonedDateTime;
 
 public class ApiPerson {
     public Person getApiPersonFromRequest() throws IOException, InterruptedException {
@@ -19,6 +22,19 @@ public class ApiPerson {
 
     public Person parcePersonRespon(HttpResponse<String> response){
         Person person = new Person();
+
+        JSONObject fullData = new JSONObject(response).
+                getJSONArray("results").getJSONObject(0);
+        person.setGender(fullData.getString("gender"));
+        person.setFirstName(fullData.getJSONObject("name").getString("first"));
+        person.setLastName(fullData.getJSONObject("name").getString("last"));
+        person.setEmail(fullData.getString("email"));
+        person.setCountry(fullData.getJSONObject("location").getString("country"));
+
+        ZonedDateTime zonedDateTime = ZonedDateTime.parse(fullData.getJSONObject("dob").
+                getString("date"));
+        person.setDob(zonedDateTime.toLocalDateTime());
+
 
 
         return person;
